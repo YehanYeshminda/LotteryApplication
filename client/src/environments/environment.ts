03e94618466d085c116eb98c3adf998b9562938e
Yehan Yeshminda <yeshanyesh12@gmail.com>
2023-09-06 10:21:20 +0400

    added the changes for the auth

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
 	// apiUrl: 'http://192.168.0.222/lotteryapi/api/',
 	apiUrl: 'http://aruna007-002-site25.etempurl.com/api/',
 	// apiUrl: 'https://localhost:5001/api/',
-	production: true,
+	production: false,
 	stripe: {
 		secretKey: 'sk_test_51NSgQVCGctxV4GttVMv6mI5lF9WhKl5PUuiNdwMenpnX3obvLwfCw7OAdNZ5uj89NuWX2qshvQ0RAsOl1GwsVG0m00UNay7K81',
 		clientPublicKey: 'pk_test_51NSgQVCGctxV4GttmBY8TDMVypTxkWkWqc8w8AfeEFXDRYv93CoqNnSLOuClW6rCevuODvzwLXGEoNj2PYRBTVIU00qutXHWvA'
